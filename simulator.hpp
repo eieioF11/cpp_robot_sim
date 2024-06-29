dd242b9c093b09c2b2c1e36a956859a6fdcac9e7
f11 <eieioeiji0501@gmail.com>
2024-06-30 00:21:33 +0900

    [fix]警告が出ないように修正

--- simulator.hpp
+++ simulator.hpp
@@ -16,63 +16,55 @@
 #include <Eigen/Dense>
 // matplotlibcpp17
 #include <matplotlibcpp17/pyplot.h>
+// OpenMP
+#include <omp.h>
 //
 #include "model.hpp"
 
-namespace cpp_robot_sim
-{
-  class simulator
-  {
+namespace cpp_robot_sim {
+  class simulator {
   private:
-    std::chrono::system_clock::time_point now, old;
+    double pre_time_;
     std::function<state_t(state_t, control_t, double)> f_;
     double width_;
     double height_;
     std::vector<double> x_{0};
     std::vector<double> y_{0};
-    matplotlibcpp17::pyplot::PyPlot plt_;
 
   public:
     state_t x_t;
-    simulator(const matplotlibcpp17::pyplot::PyPlot &plt, std::function<state_t(state_t, control_t, double)> f, double width, double height)
-    {
-      f_ = f;
-      this->now = std::chrono::system_clock::now();
-      this->old = std::chrono::system_clock::now();
-      width_ = width;
-      height_ = height;
-      plt_ = plt;
+    simulator(std::function<state_t(state_t, control_t, double)> f, double width, double height) {
+      f_        = f;
+      width_    = width;
+      height_   = height;
+      pre_time_ = omp_get_wtime();
     }
-    double deltaT()
-    {
-      this->now = std::chrono::system_clock::now();
-      double dt = std::chrono::duration_cast<std::chrono::seconds>(this->now - this->old).count();
-      this->old = this->now;
+    double deltaT() {
+      double now_time = omp_get_wtime();
+      double dt       = now_time - pre_time_;
+      pre_time_       = now_time;
       return dt;
     }
-    void reset()
-    {
+    void reset() {
       x_t << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
       x_.clear();
       y_.clear();
       x_.push_back(x_t(3));
       y_.push_back(x_t(4));
     }
-    void update(control_t v_t, double dt)
-    {
+    void update(control_t v_t, double dt) {
       x_t = f_(x_t, v_t, dt);
       x_.push_back(x_t(3));
       y_.push_back(x_t(4));
       std::cout << "control_input: " << v_t.transpose() << ", dt:" << dt << std::endl;
       std::cout << "x_t: " << x_t.transpose() << std::endl;
     }
-    void draw(bool draw_trajectory = false)
-    {
-      double x = x_t(3);
-      double y = x_t(4);
+    void draw(matplotlibcpp17::pyplot::PyPlot& plt, bool draw_trajectory = false) {
+      double x     = x_t(3);
+      double y     = x_t(4);
       double theta = x_t(5);
-      double h_w = width_ / 2.;
-      double h_h = height_ / 2.;
+      double h_w   = width_ / 2.;
+      double h_h   = height_ / 2.;
       Eigen::Matrix<double, 2, 1> po, p1, p2, p3, p4, pf;
       po << x, y; // ロボット中心座標
       p1 << h_w, h_h;
@@ -92,13 +84,12 @@ namespace cpp_robot_sim
       // 描画
       std::vector<double> x_data = {p1(0), p2(0), p3(0), p4(0), p1(0)};
       std::vector<double> y_data = {p1(1), p2(1), p3(1), p4(1), p1(1)};
-      plt_.plot(Args(x_data, y_data), Kwargs("color"_a = "black", "linewidth"_a = 1.0));
-      plt_.plot(Args(std::vector<double>({x, pf(0)}), std::vector<double>({y, pf(1)})),
-                Kwargs("color"_a = "black", "linewidth"_a = 2.0, "marker"_a = "o"));
-      plt_.plot(Args(x, y), Kwargs("color"_a = "red", "linewidth"_a = 2.0, "marker"_a = "o"));
-      if (draw_trajectory)
-      {
-        plt_.plot(Args(x_, y_), Kwargs("color"_a = "red", "linewidth"_a = 1.0));
+      plt.plot(Args(x_data, y_data), Kwargs("color"_a = "black", "linewidth"_a = 1.0));
+      plt.plot(Args(std::vector<double>({x, pf(0)}), std::vector<double>({y, pf(1)})),
+               Kwargs("color"_a = "black", "linewidth"_a = 2.0, "marker"_a = "o"));
+      plt.plot(Args(x, y), Kwargs("color"_a = "red", "linewidth"_a = 2.0, "marker"_a = "o"));
+      if (draw_trajectory) {
+        plt.plot(Args(x_, y_), Kwargs("color"_a = "red", "linewidth"_a = 1.0));
       }
     }
   };
